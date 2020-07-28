740749f46ff7b996d549897d3554f19b021bc028
gpspelle <gpsunicamp016@gmail.com>
2020-07-28 19:41:31 +0200

    create base graph

--- main.go
+++ main.go
@@ -3,52 +3,235 @@ package main
 import (
 	"fmt"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
+func process_hashtag_string(hashtag string) []string {
+
+	hashtag = strings.Replace(hashtag, "[", "", -1)
+	hashtag = strings.Replace(hashtag, "]", "", -1)
+	hashtag = strings.Replace(hashtag, "'", "", -1)
+	hashtag = strings.Replace(hashtag, " ", "", -1)
+
+	return strings.Split(hashtag, ",")
+}
+
+func read_csv(filename string) *csv.Reader {
+
+	// Open the file
+	csvfile, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+
+	fmt.Printf("%T\n", r)
+
+	r.FieldsPerRecord = -1
+
+	return r
+}
 
 func main() {
 
-	err := call_it()
+
+	r := read_csv("tweet_data.csv")
+	session, driver, err := open_connection("neo4j", "neo4j")
 
 	if err != nil {
-		fmt.Println("Error not nil")
+		log.Fatalln("Failed to connect to neo4j", err)
 	}
-}
+
+	sess := *session
+	dri := *driver
+
+	hashtag_map := make(map[string]bool)
+	user_map := make(map[string]bool)
+
+	// Iterate through the records
+	index := 0
+	for {
+		index += 1
+
+		// Read each record from csv
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tweet_id := record[0]
+		err = add_tweet_node(sess, tweet_id)
+
+		if err != nil {
+			log.Fatalln("Failed to write tweet in the database", err)
+		}
+
+		user_id := record[1]
+		_, found := user_map[user_id]  // found == true
+
+		// Only add never added nodes
+		if !found {
+			user_map[user_id] = true
+			err = add_user_node(sess, user_id)
+
+			if err != nil {
+				log.Fatalln("Failed to write user in the database", err)
+			}
+		}
 
 
-func call_it () error {
+		hashtag_string := record[2]
+		hashtags := process_hashtag_string(hashtag_string)
 
-	configForNeo4j40 := func(conf *neo4j.Config) { conf.Encrypted = false }
-	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("neo4j", "neo4j", ""), configForNeo4j40)
+		for _, hashtag := range hashtags {
+
+			if len(hashtag) == 0 {
+				continue;
+			}
+			_, found = hashtag_map[hashtag]  // found == true
+
+			// Only add never added nodes
+			if !found {
+				hashtag_map[hashtag] = true
+				err = add_hashtag_node(sess, hashtag)
+
+				if err != nil {
+					log.Fatalln("Failed to write hashtag in the database", err)
+				}
+
+			}
+
+			err = add_hashuser_relation(sess, user_id, hashtag)
+
+			if err != nil {
+				log.Fatalln("Failed to write user-hashtag relation in the database", err)
+			}
+
+		}
+
+		if index % 500 == 0 {
+			fmt.Printf("Created %d tweet nodes\n", index)
+		}
+	}
+
+	fmt.Printf("Created %d nodes\n", index)
+
+
+	/*err = delete_all(sess)
+
 	if err != nil {
-		return err
+		log.Fatalln("Failed to delete nodes", err)
 	}
+	*/
 
+	sess.Close()
 	// handle driver lifetime based on your application lifetime requirements
 	// driver's lifetime is usually bound by the application lifetime, which usually implies one driver instance per application
-	defer driver.Close()
+	dri.Close()
+
+}
+
+func open_connection(user string, passwd string) (*neo4j.Session, *neo4j.Driver, error) {
+
+	configForNeo4j40 := func(conf *neo4j.Config) { conf.Encrypted = false }
+	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth(user, passwd, ""), configForNeo4j40)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	// For multidatabase support, set sessionConfig.DatabaseName to requested database
 	sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite}
 	session, err := driver.NewSession(sessionConfig)
-	if err != nil {
-		return err
-	}
-	defer session.Close()
-
-	result, err := session.Run("CREATE (n:Item { id: $id, name: $name }) RETURN n.id, n.name", map[string]interface{}{
-		"id":   1,
-		"name": "Item 1",
-	})
 
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 
-	for result.Next() {
-		fmt.Printf("Created Item with Id = '%d' and Name = '%s'\n", result.Record().GetByIndex(0).(int64), result.Record().GetByIndex(1).(string))
+
+	return &session, &driver, nil
+
+}
+
+func add_tweet_node (session neo4j.Session, tweet_id string) error {
+
+
+	attributes := map[string]interface{} {"tweet_id": tweet_id}
+	result, err := session.Run("CREATE (n:Tweet { tweet_id: $tweet_id })", attributes)
+
+	if err != nil {
+		return err
 	}
 
 	return result.Err()
-
 }
+
+func add_user_node (session neo4j.Session, user_id string) error {
+
+
+	attributes := map[string]interface{} {"user_id": user_id}
+	result, err := session.Run("CREATE (n:User { user_id: $user_id })", attributes)
+
+	if err != nil {
+		return err
+	}
+
+	return result.Err()
+}
+
+func add_hashtag_node (session neo4j.Session, hashtag string) error {
+
+	attributes := map[string]interface{} {"hashtag": hashtag}
+	result, err := session.Run("CREATE (n:Hashtag { hashtag: $hashtag })", attributes)
+
+	if err != nil {
+		return err
+	}
+
+	return result.Err()
+}
+
+func add_hashuser_relation(session neo4j.Session, user_id string, hashtag string) error {
+
+	attributes := map[string]interface{} {"user_id": user_id, "hashtag": hashtag}
+	result, err := session.Run("MATCH (a:User),(b:Hashtag) WHERE a.user_id = $user_id AND b.hashtag = $hashtag CREATE (a)-[r:POST]->(b)", attributes)
+
+	if err != nil {
+		return err
+	}
+
+	return result.Err()
+}
+
+func delete_all_nodes(session neo4j.Session) error {
+
+	result, err := session.Run("MATCH (n) DELETE n", map[string]interface{}{})
+
+	if err != nil {
+		return err
+	}
+
+	return result.Err()
+}
+
+func delete_all(session neo4j.Session) error {
+
+	result, err := session.Run("MATCH (n) DETACH DELETE n", map[string]interface{}{})
+
+	if err != nil {
+		return err
+	}
+
+	return result.Err()
+}
+
